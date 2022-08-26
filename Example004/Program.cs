93df7de3f3ed019361e0788ba2f28b992ee8d385
Евгений <ivanychcos@gmail.com>
2022-08-26 14:20:52 +0300

    Add Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+
+if(x % 7 ==0 && x % 23 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
