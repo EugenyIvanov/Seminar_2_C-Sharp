b60882974fe84e347412692dab687d3ef7e40be6
Евгений <ivanychcos@gmail.com>
2022-08-26 15:02:56 +0300

    Add example 005

---
+++ Example005/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+int max = a;
+int min = b;
+
+if(a < b)
+{
+    max = b;
+    min = a;
+}
+if(min * min == max)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    {Console.WriteLine("No");
+    }
+}
