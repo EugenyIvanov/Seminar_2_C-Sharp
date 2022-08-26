8f2fb8a6fb0e21ecaa3e001a32e3cffd6d7f22c1
Евгений <ivanychcos@gmail.com>
2022-08-26 13:21:32 +0300

    Add example 002

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,14 +1,13 @@
 ﻿Console.Clear();
 
-int num = new Random().Next(10, 100);
+int num = new Random().Next ( 10, 100 );
 Console.WriteLine(num);
 
 int a1 = num / 10;
 int a2 = num % 10;
 
 int max = a1;
-
-if(a2 > max)
+if (a2 > max)
 {
     max = a2;
 }
