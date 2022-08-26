8d871e997b76e4dbec5f68430a9d7c1827320501
Евгений <ivanychcos@gmail.com>
2022-08-26 13:47:27 +0300

    Add Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int a = new Random().Next(10, 100);
+int b = new Random().Next(1, 10);
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+if (a % b == 0)
+{
+    Console.Write("Кратно");
+}
+else
+{
+    int x = a % b;
+    Console.Write("Не кратно, остаток ");
+    Console.Write(x);
+}
